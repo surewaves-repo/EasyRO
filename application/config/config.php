c1c051a431edc331207e857bee46fd717ee7218c
root <root@ip-192-168-3-140.ap-southeast-1.compute.internal>
2024-09-02 16:06:37 +0530

    changed config file for test env

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url'] = 'http://easyro.surewaves.com/surewaves_easy_ro';
+$config['base_url'] = 'https://test-easyro.surewaves.com/surewaves_easy_ro';
 
 /*
 |--------------------------------------------------------------------------
